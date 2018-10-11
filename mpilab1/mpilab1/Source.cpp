902adce94018978add6863e79a29c38c18d38523
VadimKutovoi <sir.kutovoi@yandex.ru>
2018-10-11 21:14:39 +0300

    Moved generation to MPI block;added root process sum saving

--- mpilab1/mpilab1/Source.cpp
+++ mpilab1/mpilab1/Source.cpp
@@ -12,12 +12,7 @@ void main(int argc, char *argv[])
 	matrix = new int*[rows];
 	sum = new int[rows];
 
-	for (int i = 0; i < rows; i++) {
-		matrix[i] = new int[columns];
-		sum[i] = 0;
-		for (int j = 0; j < columns; j++)
-			matrix[i][j] = i + j;
-	}
+	
 
 	MPI_Init(&argc, &argv);
 
@@ -28,6 +23,20 @@ void main(int argc, char *argv[])
 	MPI_Bcast(&columns, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 	if (rank == 0) {
+		for (int i = 0; i < rows; i++) {
+			matrix[i] = new int[columns];
+			sum[i] = 0;
+			for (int j = 0; j < columns; j++)
+				matrix[i][j] = i + j;
+		}
+
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < columns; j++)
+			{
+				printf("%i ", matrix[i][j]);
+				if (j == columns - 1) printf("\n");
+			}
+
 		for (int i = 0; i < rows; i++) {
 			if (i%size != 0)
 				MPI_Send(matrix[i], columns, MPI_INT, i%size, i, MPI_COMM_WORLD);
@@ -45,13 +54,24 @@ void main(int argc, char *argv[])
 		for (int i = 0; i < columns; i++)
 			row_sum += recv_row[i];
 		
-		/*MPI_Send(&row_sum, 1, MPI_INT, 0, rank, MPI_COMM_WORLD);*/
-		printf("Process %i send sum = %i ", rank, row_sum);
+		MPI_Send(&row_sum, 1, MPI_INT, 0, rank, MPI_COMM_WORLD);
+		printf("Process %i send sum = %i\n", rank, row_sum);
 	}
 	else {
 		for (int i = 0; i < columns; i++)
 			sum[0] += matrix[0][i];
-			printf("Process %i send sum = %i ", rank, sum[0]);
+			printf("Process %i send sum = %i\n", rank, sum[0]);
+	}
+
+	if (rank == 0) {
+		for (int i = 1; i < rows; i++) {
+			MPI_Recv(&sum[i], 1, MPI_INT, MPI_ANY_SOURCE, i, MPI_COMM_WORLD, &status);
+			
+		}
+
+		for (int i = 0; i < rows; i++) {
+			printf("sum[%i] = %i\n", i, sum[i]);
+		}
 	}
 
 	MPI_Finalize();
