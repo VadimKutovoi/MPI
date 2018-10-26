e5bc66cae7afdbbb2256ee46f037bf08883b6863
VadimKutovoi <sir.kutovoi@yandex.ru>
2018-10-26 16:10:02 +0300

    Added several changes

--- mpilab1/mpilab1/Source.cpp
+++ mpilab1/mpilab1/Source.cpp
@@ -3,6 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+void generate_matr(double* matr,int row_c, int column_c) {
+	for (int i = 0; i < row_c * column_c; i++) {
+		matr[i] = rand() % 10;
+	}
+}
+
+void print_matr(double* matr, int row_c, int column_c) {
+	for (int i = 1; i < row_c * column_c + 1; i++) {
+		std::cout << matr[i - 1] << " ";
+		if (i%column_c == 0) {
+			std::cout << std::endl;
+		}
+	}
+}
 
 void main(int argc, char *argv[])
 {
@@ -10,7 +24,7 @@ void main(int argc, char *argv[])
 	MPI_Init(&argc, &argv);
 
 	int rank, size, rows = 5, columns = 5;
-	int **matrix = NULL, *recv_row = NULL, *sum = NULL;
+	double *matrix = NULL, *recv_row = NULL, *sum = NULL;
 	double times;
 	MPI_Status status;
 
@@ -28,64 +42,50 @@ void main(int argc, char *argv[])
 
 		std::cout << "Generating matrix.." << std::endl;
 
-		matrix = new int*[rows];
-		sum = new int[rows];
+		matrix = new double[columns * rows];
 
-		for (int i = 0; i < rows; i++) {
-			matrix[i] = new int[columns];
-			sum[i] = 0;
-			for (int j = 0; j < columns; j++)
-				matrix[i][j] = i + j;
-		}
+		generate_matr(matrix, rows, columns);
+		print_matr(matrix, rows, columns);
 
-		for (int i = 0; i < rows; i++)
-			for (int j = 0; j < columns; j++)
-			{
-				std::cout << matrix[i][j] << " ";
-				if (j == columns - 1) std::cout << std::endl;
-			}
-
+		times = MPI_Wtime();
 	}
 
-	times = MPI_Wtime();
 	
-	MPI_Bcast(&rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	
+	//MPI_Bcast(&rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Bcast(&columns, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	//MPI_Bcast(&matrix, columns * rows, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+	//изменить матрицу на массив, инт на дабл, передалать сенд.
 	if (rank == 0) {
-		std::cout << "Starting MPI_Send..." << std::endl;
-
 		for (int i = 0; i < rows; i++) {
 			if (i%size != 0)
-				MPI_Send(matrix[i], columns, MPI_INT, i%size, i, MPI_COMM_WORLD);
+				MPI_Send(matrix, columns, MPI_INT, i%size, i, MPI_COMM_WORLD);
 		}
-
-		std::cout << "Finished\n" << std::endl;
 	}
-	
 	if(rank != 0) {
-		recv_row = new int[columns];
+		recv_row = new double[columns];
 		for (int i = rank; i < rows; i += size) {
 			int row_sum = 0;
 
 			MPI_Recv(recv_row, columns, MPI_INT, 0, i, MPI_COMM_WORLD, &status);
-			std::cout << "Process " << rank << " recieved row" << std::endl;
+			//std::cout << "Process " << rank << " recieved row" << std::endl;
 
 			for (int j = 0; j < columns; j++)
 				row_sum += recv_row[j];
 
-			MPI_Send(&row_sum, 1, MPI_INT, 0, i, MPI_COMM_WORLD);
-			std::cout << "Process " << rank << " sended row" << std::endl;
+			MPI_Send(&row_sum, 1, MPI_INT, 0, i, MPI_COMM_WORLD); //Gather
+			//std::cout << "Process " << rank << " sended row" << std::endl;
 		}
+		delete recv_row;
 	}
 	else {
 		for (int i = rank; i < rows; i += size)
 		{
 			for (int j = 0; j < columns; j++)
 			{
-				sum[i] += matrix[i][j];
+				sum[i] += matrix[i];
 			}
-				
 		}
 	}
 
@@ -103,33 +103,34 @@ void main(int argc, char *argv[])
 		}
 	}
 
-	MPI_Finalize();
-
-	for (int j = 0; j < columns; j++) sum[j] = 0;
-
-	std::cout << std::endl;
 	//sequential program
 
-	time_t start, end;
-	
-	time(&start);
+	if (rank == 0) {
+		for (int j = 0; j < columns; j++) sum[j] = 0;
 
-	for (int i = 0; i < rows; i++)
-		for (int j = 0; j < columns; j++)
-		{
-			sum[i] += matrix[i][j];
+		std::cout << std::endl;
+		//sequential program
+
+		times = MPI_Wtime();
+
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < columns; j++)
+			{
+				sum[i] += matrix[i][j];
+			}
+	
+		std::cout << "Time = " << MPI_Wtime() - times << std::endl;
+
+		for (int i = 0; i < rows; i++) {
+			std::cout << "sum2[" << i << "] = " << sum[i] << std::endl;
 		}
 
-	time(&end);
-	double seconds = difftime(end, start);
-	std::cout << "sequential program time = " << seconds << std::endl;
-	
-	for (int i = 0; i < rows; i++) {
-		std::cout << "sum2[" << i << "] = " << sum[i] << std::endl;
+
+		for (int i = 0; i < rows; i++) {
+			delete[] matrix[i];
+		}
+		delete[] matrix, sum;
 	}
 
-	for (int i = 0; i < rows; i++) {
-		delete[] matrix[i];
-	}
-	delete[] matrix, sum, recv_row;
+	MPI_Finalize();
 }
